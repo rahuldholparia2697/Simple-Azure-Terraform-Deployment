1cf05a028bd1a1b7f649db62986f80b41ef70887
Rahul Rahul <rahul.rahul@gpw.pl>
2026-03-28 15:44:50 +0100

    Second Commit

--- Terraform-code/provider.tf
+++ Terraform-code/provider.tf
@@ -6,10 +6,19 @@ terraform {
     }
   }
 
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "Azure-DevOps-RG"
+    storage_account_name = "devstorageaccount2026"
+    container_name       = "githubcontainer"
+    key                  = "terraform.tfstate"
+
+    # Tells the backend to also use the Managed Identity
+    use_msi = true
+  }
 }
 
 provider "azurerm" {
   features {}
+  use_msi         = true
   subscription_id = "99c0acb2-ff53-4ac2-8e99-071411ed7ee4"
 }
